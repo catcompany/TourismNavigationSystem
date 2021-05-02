4b8deedcf82726f4c26ef2a55235a9f7a3d07d6e
iMorning <admin@catcompany.cn>
2021-05-02 20:47:20 +0800

    fix the bug:can't upload file.

--- mainUI/src/main/java/com/imorning/tns/ui/bbs/BBSFragment.java
+++ mainUI/src/main/java/com/imorning/tns/ui/bbs/BBSFragment.java
@@ -1,7 +1,16 @@
 package com.imorning.tns.ui.bbs;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,18 +18,53 @@ import android.view.ViewGroup;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.imorning.tns.R;
-import com.imorning.tns.webview.X5WebView;
+import com.imorning.tns.ui.SlackLoadingView;
 import com.mylhyl.circledialog.CircleDialog;
+import com.tencent.smtt.sdk.ValueCallback;
+import com.tencent.smtt.sdk.WebChromeClient;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
+
+import static android.app.Activity.RESULT_OK;
 
 public class BBSFragment extends Fragment {
+    private final static int FILE_CHOOSER_RESULT_CODE = 10000;
     private static final String TAG = "BBSFragment";
     private final static String bbsUrl = "https://bbs.catcompany.cn/";
-    private X5WebView webView;
+    private ValueCallback<Uri[]> uploadMessageAboveL;
+    private ValueCallback<Uri> uploadMessage;
+    private WebView webView;
     private OnBackPressedCallback callback;
+    //加载动画框
+    private AppCompatDialog loadingDialog;
+    private final WebViewClient client = new WebViewClient() {
+        /**
+         * 防止加载网页时调起系统浏览器
+         */
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            //要返回true否则内核会继续处理
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            showLoadingDialog();
+        }
+
+        @Override
+        public void onPageFinished(WebView webView, String s) {
+            super.onPageFinished(webView, s);
+            hideLoadingDialog();
+        }
+    };
 
     public static BBSFragment newInstance() {
         return new BBSFragment();
@@ -31,7 +75,6 @@ public class BBSFragment extends Fragment {
         new ViewModelProvider(this).get(BBSViewModel.class);
         View rootView = inflater.inflate(R.layout.bbs_fragment, container, false);
         webView = rootView.findViewById(R.id.webview_bbs);
-
         return rootView;
     }
 
@@ -39,7 +82,106 @@ public class BBSFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         new ViewModelProvider(this).get(BBSViewModel.class);
+        initWebViewSettings();
         webView.loadUrl(bbsUrl);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initWebViewSettings() {
+        webView.setWebViewClient(client);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> valueCallback, FileChooserParams fileChooserParams) {
+                uploadMessageAboveL = valueCallback;
+                openImageChooserActivity();
+                return true;
+            }
+        });
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setAllowFileAccess(true);
+        webSetting.setAllowFileAccessFromFileURLs(true);
+        webSetting.setAllowUniversalAccessFromFileURLs(true);
+        webSetting.setSupportZoom(false);
+        webSetting.setGeolocationEnabled(true);
+        webSetting.setSavePassword(true);
+        webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        // settings 的设计
+    }
+
+
+    private void openImageChooserActivity() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("*/*");
+        startActivityForResult(Intent.createChooser(i, "文件选择"), FILE_CHOOSER_RESULT_CODE);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_CHOOSER_RESULT_CODE) {
+            if (null == uploadMessage && null == uploadMessageAboveL) return;
+            Uri result = data == null || resultCode != RESULT_OK ? null : data.getData();
+            if (uploadMessageAboveL != null) {
+                onActivityResultAboveL(requestCode, resultCode, data);
+            } else if (uploadMessage != null) {
+                uploadMessage.onReceiveValue(result);
+                uploadMessage = null;
+            }
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void onActivityResultAboveL(int requestCode, int resultCode, Intent intent) {
+        if (requestCode != FILE_CHOOSER_RESULT_CODE || uploadMessageAboveL == null)
+            return;
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (intent != null) {
+                String dataString = intent.getDataString();
+                ClipData clipData = intent.getClipData();
+                if (clipData != null) {
+                    results = new Uri[clipData.getItemCount()];
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+                if (dataString != null)
+                    results = new Uri[]{Uri.parse(dataString)};
+            }
+        }
+        uploadMessageAboveL.onReceiveValue(results);
+        uploadMessageAboveL = null;
+    }
+
+
+    /**
+     * 显示加载动画
+     */
+    public synchronized void showLoadingDialog() {
+        if (loadingDialog == null)
+            loadingDialog = new AppCompatDialog(requireContext());
+        @SuppressLint("InflateParams")
+        View view = LayoutInflater.from(requireContext()).inflate(R.layout.web_loading_layout, null);
+        ((SlackLoadingView) view.findViewById(R.id.web_loading_view)).start();
+        loadingDialog.setContentView(view);
+        loadingDialog.setCancelable(false);
+        loadingDialog.create();
+        loadingDialog.show();
+    }
+
+    /**
+     * 隐藏加载动画
+     */
+    public synchronized void hideLoadingDialog() {
+        if (loadingDialog != null && loadingDialog.isShowing()) {
+            loadingDialog.dismiss();
+            loadingDialog = null;
+        }
     }
 
     @Override
@@ -53,6 +195,7 @@ public class BBSFragment extends Fragment {
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
     }
+
 
     private void webViewBack() {
         if (webView.canGoBack() && callback != null) {
